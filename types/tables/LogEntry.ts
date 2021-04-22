86d2c570fd4c2e35b00963f53b39ceff6f2cfcf4
Louis Andrew <louis.andrew@share-now.com>
2021-04-22 10:54:20 +0200

    feat(types): create types for Log and LogEntry tables

---
+++ types/tables/LogEntry.ts
@@ -0,0 +1,31 @@
+import { ExerciseTable } from './Exercise'
+import { LogTable } from './Log'
+import { Table } from './Table'
+
+export type LogEntryTable = Table & {
+  /**
+   * id of the exercise logged or its
+   */
+  id: number | ExerciseTable
+  /**
+   * stringified version of a LogEntry object
+   * @see LogEntry
+   */
+  log: string
+}
+
+export type EntriesTable = Table & {
+  /**
+   * id of the exercise log / its data
+   */
+  log_id: number | LogTable
+  /**
+   * id of the entry / its data
+   */
+  entry_id: number | LogEntryTable
+  /**
+   * order of the log entry within an exercise log
+   * ! Starts with 0
+   */
+  order: number
+}
