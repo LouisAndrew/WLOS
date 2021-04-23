f57b037b9b6f22dc06aa43804dba2519703e653f
Louis Andrew <louis.andrew@share-now.com>
2021-04-23 22:12:40 +0200

    feat(css-module): add config

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react'
 
 import { NavLink } from './nav-link'
-import styles from './styles.module.css'
+import styles from './nav.module.css'
 
 export type Props = {}
 
