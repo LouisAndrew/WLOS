e9790854ed54ad6a38638cc5d94e33f98ef38c45
Louis Andrew <louis.andrew@share-now.com>
2021-04-24 16:57:52 +0200

    feat(avatar): add boilerplate avatar component

---
+++ components/nav/avatar/avatar.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+import styles from './avatar.module.css'
+
+export type props = {}
+
+// TODO: fetch avatar from user data.
+const Avatar = () => {
+  return <div className={styles.container}></div>
+}
+
+export default Avatar
