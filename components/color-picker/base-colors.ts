099376b3a1578b4f960ef1e884f25426cd6c530e
Louis Andrew <louis.andrew@share-now.com>
2021-04-29 09:55:12 +0200

    feat(color-picker): create component logic

---
+++ components/color-picker/base-colors.ts
@@ -0,0 +1,28 @@
+// tailwind colors on (500)
+export const colors = [
+  'red',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'pink',
+  'black',
+  'white',
+] as const
+export type Color = typeof colors[number]
+export type ColorCode = Record<Color, string>
+export const colorCodes: ColorCode = {
+  red: '#EF4444',
+  yellow: '#F59E0B',
+  green: '#10B981',
+  blue: '#3B82F6',
+  indigo: '#6366F1',
+  pink: '#EC4899',
+  black: '#0E0E0E',
+  white: '#FFFFFF',
+}
+
+export const colorTable: [Color, string][] = Object.keys(colorCodes).map((key) => [
+  key as Color,
+  colorCodes[key],
+])
