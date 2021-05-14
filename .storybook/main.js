3d1c4e76944bf47c7da1879e49bc08530fd7ac47
Louis Andrew <louis.andrew@share-now.com>
2021-05-14 23:02:54 +0200

    fix(storybook): dirty commit. fix storybook config and metric-input popup css

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,39 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import'),
+                require('tailwindcss'),
+                require('postcss-nested'), // or require('postcss-nesting')
+                require('autoprefixer'),
+              ],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@c': path.resolve(__dirname, '../components/'),
+      '@lib': path.resolve(__dirname, '../lib/'),
+      '@h': path.resolve(__dirname, '../hooks/'),
+      '@t': path.resolve(__dirname, '../types/'),
+      "@v": path.resolve(__dirname, "../views/"),
+      '@': path.resolve(__dirname, '../'),
+    }
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-css-modules-preset', 'storybook-dark-mode'],
 }
